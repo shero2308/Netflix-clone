825303946141788c5a8aa3c9242b955385d6aa65
Rakshit <rakshitkumar682@gmail.com>
2025-05-16 17:23:52 +0530

    Implement code changes to enhance functionality and improve performance

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import "./TitleCards.css"
+import card_data from '../../assets/cards/Cards_data'
 
 const TitleCards = () => {
+  const CardsRef = useRef();
+
+const Handlewheel = (e) => {
+  e.preventDefault();
+  CardsRef.current.scrollLeft += e.deltaY;
+}
+
+useEffect(()=>{
+  CardsRef.current.addEventListener('wheel', Handlewheel)
+},[])
   return (
     <div className='titleCards'>
-      
+      <h2>Popular on Netflix</h2>
+      <div className="card-list" ref={CardsRef}>
+        {card_data.map((card ,index)=>{
+          return <div className="card" key={index}>
+            <img src={card.image} alt="" />
+            <p>{card.name}</p>
+          </div>
+        })}
+      </div>
     </div>
   )
 }
