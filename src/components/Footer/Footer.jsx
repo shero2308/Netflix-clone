2f35d17785a8b7db309c1f093271cffe102b9f2b
Rakshit <rakshitkumar682@gmail.com>
2025-07-19 12:40:03 +0530

    das

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -28,7 +28,7 @@ const Footer = () => {
         <li>Corporate Information</li>
         <li>Contact Us</li>
       </ul>
-      <p className='copyright-text'></p>
+      <p className='copyright-text'>@ 1997-2025 Netflix, Inc.</p>
     </div>
   )
 }
