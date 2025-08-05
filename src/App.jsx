6bfb8e6583ec6e29ddbb1938b3c5a33090248ffe
Rakshit <rakshitkumar682@gmail.com>
2025-08-05 13:27:32 +0530

    Fix routing for Login page and update import statement

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Home from './pages/Home/Home'
 import { Routes ,Route } from 'react-router-dom'
-import Login from './pages/Login/Login'
+import Login from './pages/Login/Login.jsx'
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/" element={<Login/>} />
+        <Route path="/login" element={<Login/>} />
       </Routes>
     </div>
   )
