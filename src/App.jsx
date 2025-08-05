a25c94e7011259221605c0beb7c4143ce91ab467
Rakshit <rakshitkumar682@gmail.com>
2025-08-05 12:34:44 +0530

    Add React Router for navigation and update dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Home from './pages/Home/Home'
+import { Routes ,Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   )
 }
